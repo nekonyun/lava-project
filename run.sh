8f67a28aca5e459888188d47217fa3cac885d98a
panji kurniawan <panjikun00@gmail.com>
2024-03-23 20:19:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4c3da4f2-4162-4c84-a979-0208b47d3d98/)
 
 
 echo "$curl_output" >> Update.tsx
